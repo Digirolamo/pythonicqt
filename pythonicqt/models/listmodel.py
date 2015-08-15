6e6b4eeb6d31e274a8922ed4feb210ae79c6ed7c
Chris diGirolamo <chrisdigirolamo@gmail.com>
2015-08-14 22:08:50 -0400

    Add listmodel.py and extra documentation to the former module.

--- pythonicqt/models/listmodel.py
+++ pythonicqt/models/listmodel.py
@@ -3,5 +3,148 @@ Data structures and class that acts like a python list and a Qt QListModel.
 """
 from PySide import QtCore
 
+class ItemMetaData(dict):
+    """Contains flags and roles"""
+
 class ListModel(QtCore.QAbstractListModel):
-    """"""
+    #TODO DOCS
+    """
+    Signals Emited:
+    ListChanged(int, object): the index changed, and the value before the change.
+    ListCleared(): When the entire list is cleared. List Changed is not called in this case."""
+    ListChanged = QtCore.Signal(int, object) 
+    ListCleared = QtCore.Signal() 
+
+    default_roles = {}
+    def __init__(self, container=None):
+        QtCore.QAbstractListModel.__init__(self)
+        if container is None:
+            container = []
+        self.container = container
+        self.container_meta = [ItemMetaData() for e in xrange(len(container))] #index to role/flag index
+        
+    #QListModel virtual methods
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        if orientation == QtCore.Qt.Vertical:
+            return unicode(section)
+        else:
+            return unicode(section)
+    
+    def rowCount(self, parent=None):
+        """Returns the length of the underlying list."""
+        if parent is None:
+            return 0
+        return len(self.container)
+    
+    def columnCount(self, parent=None):
+        """Returns 1 because lists do not have multiple columns."""
+        return 1
+    
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if not index.isValid():
+            return None
+        row, column = index.row(), index.column()
+        if role == QtCore.Qt.DisplayRole:
+            return unicode(self.container[row])
+        item_metadata = self.container_meta[row]
+        try:
+            return item_metadata[role]
+        except KeyError as e:
+            return None
+        
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if not index.isValid():
+            return None
+        row, column = index.row(), index.column()
+        if role == QtCore.Qt.EditRole:
+            self[row] = value
+            
+    def set_default_meta(self, role, value):
+        """"""
+        
+    def set_meta(self, idx, role, value):
+        """Returns a list that...djskfsdf"""
+        self.container_meta[idx][role] = value
+        index = self.index(idx, 0)
+        self.dataChanged.emit(index, index)
+
+    # Python Sequence Methods
+    def __len__(self, *args):
+        return self.container.__len__(*args)
+    
+    def __contains__(self, *args):
+        return self.container.__contains__(*args)
+    
+    def __delitem__(self, idx):
+        previous = self.container[idx]
+        parent = QtCore.QModelIndex()
+        self.beginRemoveRows(parent, idx, idx) 
+        ret_value = self.container.__delitem__(idx)
+        self.container_meta.__delitem__(idx)
+        self.endRemoveRows()
+        self.ListChanged.emit(idx, previous)
+        return ret_value
+
+    def __getitem__(self, idx):
+        return self.container.__getitem__(idx)
+
+    def __setitem__(self, idx, value):
+        previous = self.container[idx]
+        ret_value = self.container.__setitem__(idx, value)
+        index = self.index(idx, 0)
+        self.dataChanged.emit(index, index)
+        self.ListChanged.emit(idx, previous)
+        return ret_value
+
+    def remove(self, item):
+        """Removes the first occurence of an item."""
+        idx = self.container.index(item)
+        del self[idx]
+
+    def clear(self):
+        """Clears entire model and emits layout changed"""
+        self.layoutAboutToBeChanged.emit()
+        self.container = self.container[:]
+        self.layoutChanged.emit()
+        self.ListCleared.emit()
+
+    def insert(self, idx, value):
+        previous = None
+        try:
+            previous = self.container[idx]
+        except IndexError:
+            pass
+        parent = QtCore.QModelIndex()
+        self.beginInsertRows(parent, idx, idx)
+        ret_value = self.container.insert(idx, value)
+        self.container_meta.insert(idx, ItemMetaData())
+        self.endInsertRows()
+        self.ListChanged.emit(idx, previous)
+        return ret_value
+
+    def append(self, value):
+        return self.insert(len(self.container), value)
+
+    def get_list(self):
+        """Returns a new python list version of the container."""
+        return list(self.container)
+
+    #TODO methods
+    def __setslice__ (self, *args):
+        """We disable slice setting until we can handle
+        alerting the datamodel of the changes"""
+        raise NotImplementedError("No slice setting functionality.")
+        
+    def __delslice__ (self, *args):
+        """We disable slice deleting until we can handle
+        alerting the datamodel of the changes"""
+        raise NotImplementedError("No slice deleting functionality.")
+
+    def __str__(self):
+        return self.container.__str__()
+    
+    def __repr__(self):
+        """The representation does not include any of the metadata."""
+        sequence = self.container.__repr__()
+        return "{}({})".format(self.__class__.__name__,
+                                        sequence)

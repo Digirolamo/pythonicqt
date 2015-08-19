4adeba8a2f808badc678a5ff421daa78a971249b
Chris Digirolamo <chrisdigirolamo@gmail.com>
2015-08-18 22:17:21 -0400

    Refactore ListModel to be a MutableSequence.

---
+++ pythonicqt/baseclasses.py
@@ -0,0 +1,8 @@
+"""This module contains the base classes used by modules
+throughout the pythonicqt package."""
+from abc import ABCMeta
+from PySide import QtCore
+
+class QtMetaStitch(ABCMeta, type(QtCore.QObject)):
+    """In order to combine Qt types and object types we
+    need to stitch the metaclasses together."""
--- pythonicqt/models/listmodel.py
+++ pythonicqt/models/listmodel.py
@@ -1,22 +1,22 @@
 """This module contains the Data structures and classes
 that are used to impliment a python list like QAbstractListmodel.
 """
+from abc import ABCMeta
+from collections import MutableSequence
 from six.moves import xrange
 from PySide import QtCore
+from pythonicqt.baseclasses import QtMetaStitch
 
-
-class ItemMetaData(dict):
+class ItemRoleData(dict):
     """Contains flags and roles"""
 
 
-class ListModel(QtCore.QAbstractListModel):
-    """A python list implimentation of a QAbstractListModel.
+class BasicListModel(QtCore.QAbstractListModel):
+    """An implimentation of a QAbstractListModel using a python list.
 
-    Instances of this class function as both a QAbstractListModel and
-    a python list. This allows you to interact with the object like any
-    other python list through convenient methods and index access, but
-    with the benifit of updating all connected views. The connected
-    Qt views can also edit and change the list.
+        self._container is a python list that holds the data of this
+        QAbstractListModel. You can use this model as-is, but its
+        primary purpose is the ListModel subclass of it.
 
     Note:
         This object emits two signals in addition to the signals emited by
@@ -35,27 +35,21 @@ class ListModel(QtCore.QAbstractListModel):
         container (Optional[list]): The first parameter is a list if you want to instantiate
             an instance with starting values. If provided, a copy of the list is created.
     """
-    ListChanged = QtCore.Signal(int, object) 
-    ListCleared = QtCore.Signal() 
-
-    default_flags = (QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable | 
-                     QtCore.Qt.ItemIsDragEnabled |  QtCore.Qt.ItemIsSelectable)
-    default_roles = {}
-
+    #So future objects can inherit
+    __metaclass__ = QtMetaStitch
+    
     def __init__(self, container=None):
         QtCore.QAbstractListModel.__init__(self)
         if container is None:
             container = []
         self._container = container
-        self.container_meta = [ItemMetaData() for e in range(len(container))] #index to role/flag index
-        
+        self.container_meta = [ItemRoleData() for e in range(len(container))] #index to role/flag index
+
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
         """Class methods are similar to regular functions.
-
         Note:
             This method overrides the virtual function of it's parent.
         """
-
         if orientation == QtCore.Qt.Vertical:
             return unicode(section)
         else:
@@ -63,7 +57,6 @@ class ListModel(QtCore.QAbstractListModel):
 
     def rowCount(self, parent=None):
         """Returns the length of the underlying list.
-
         Note:
             This method overrides the virtual function of it's parent.
         """
@@ -73,7 +66,6 @@ class ListModel(QtCore.QAbstractListModel):
     
     def columnCount(self, parent=None):
         """Returns 1 because lists do not have multiple columns.
-
         Note:
             This method overrides the virtual function of it's parent.
         """
@@ -81,16 +73,13 @@ class ListModel(QtCore.QAbstractListModel):
     
     def data(self, index, role=QtCore.Qt.DisplayRole):
         """Returns the data of a role at a specific index of the list.
-
         If the role is DisplayRole or EditRole, returns the value located
         in the underlying list at the index. Else returns the role of the
         individual item if the user has set it. Else returns the default
         role data located in the class attribute if it exists.
-
         Args:
             index (QModelIndex):
             role (Optional[ItemRole]):
-
         Note:
             This method overrides the virtual function of it's parent.
         """
@@ -107,15 +96,12 @@ class ListModel(QtCore.QAbstractListModel):
 
     def setData(self, index, value, role=QtCore.Qt.EditRole):
         """Sets the data of a role at a specific index of the list.
-
         If the role is DisplayRole or EditRole, sets the value located
         in the underlying list at the index. Else sets the role of the
         individual item specifically.
-
         Args:
             index (QModelIndex):
             role (Optional[ItemRole]):
-
         Note:
             This method overrides the virtual function of it's parent.
         """
@@ -151,8 +137,129 @@ class ListModel(QtCore.QAbstractListModel):
         index = self.index(idx, 0)
         self.dataChanged.emit(index, index)
 
-    # Python Special Methods
 
+class ListModel(MutableSequence, BasicListModel):
+    """A python list like implimentation of a QAbstractListModel.
+    
+    Instances of this class function as both a QAbstractListModel and
+    a python list (MutableSequence specifically). This allows you to 
+    interact with the object like any other python list through convenient
+    methods and index access, but with the benifit of updating all 
+    connected views. The connected Qt views can also edit and change the 
+    list.
+    
+    Note:
+        ListModel.index_of is the method for MutableSequence.index. 
+        ListModel.index is the QAbstractListModel.index method.
+        
+    """
+    ListChanged = QtCore.Signal(int, object) 
+    ListCleared = QtCore.Signal() 
+
+    default_flags = (QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable | 
+                     QtCore.Qt.ItemIsDragEnabled |  QtCore.Qt.ItemIsSelectable)
+    default_roles = {}
+
+    def _convert_idx(self, idx):
+        """To be compatable with Qt objects, ensure the index is a positive number.
+        Used for python index operations."""
+        if idx < 0:
+            #idx -1 on a length 10 list is the same as idx 9 (10 + -1)
+            idx = len(self) + idx
+        return idx
+
+    #MutableSequence Abstract Methods
+    def __getitem__(self, idx):
+        """Gets the data located the the index or slice in the underlying list.
+        
+        Note:
+            This method is an abstract method required for MutableSequence.
+        """
+        idx = self._convert_idx(idx)
+        return self._container.__getitem__(idx)
+
+    def __setitem__(self, idx, value):
+        """Sets the data located the the index or slice in the underlying list.
+        
+        Note:
+            This method is an abstract method required for MutableSequence.
+        """
+        idx = self._convert_idx(idx)
+        idx = self._convert_idx(idx)
+        previous = self._container[idx]
+        ret_value = self._container.__setitem__(idx, value)
+        index = self.index(idx, 0)
+        self.dataChanged.emit(index, index)
+        self.ListChanged.emit(idx, previous)
+        return ret_value
+
+    def __delitem__(self, idx):
+        """Deletes an item from the underlying list and any associated metadata,
+        then updates the model.
+        
+        Note:
+            This method is an abstract method required for MutableSequence.
+        """
+        idx = self._convert_idx(idx)
+        previous = self._container[idx]
+        parent = QtCore.QModelIndex()
+        self.beginRemoveRows(parent, idx, idx) 
+        ret_value = self._container.__delitem__(idx)
+        self.container_meta.__delitem__(idx)
+        self.endRemoveRows()
+        self.ListChanged.emit(idx, previous)
+        return ret_value
+    
+    def __len__(self, *args):
+        """Returns the length of the underlying list.
+        
+        Note:
+            This method is an abstract method required for MutableSequence.
+        """
+        return self._container.__len__(*args)
+    
+    def insert(self, idx, value):
+        """Inserts data into the underlying list.
+
+        Note:
+            This method is an abstract method required for MutableSequence."""
+        previous = None
+        try:
+            previous = self._container[idx]
+        except IndexError:
+            pass
+        parent = QtCore.QModelIndex()
+        self.beginInsertRows(parent, idx, idx)
+        ret_value = self._container.insert(idx, value)
+        self.container_meta.insert(idx, ItemRoleData())
+        self.endInsertRows()
+        self.ListChanged.emit(idx, previous)
+        return ret_value
+ 
+    #Index needs to use QAbstractListModel Index
+    def index(self, row, column, parent=QtCore.QModelIndex()):
+        """Returns the QModelIndex from the underlying model.
+        
+        Note:
+            This method overrides the virtual function of QAbstractListModel.
+            This is not the MutableSequence.index method. That is index_of.
+        """
+        return QtCore.QAbstractListModel.index(self, row, column, parent=parent)
+    
+    #QListModel index method overrides Sequence index method
+    def index_of(self, item):
+        """Calls the python version of list.index.
+        Returns the index of the item that matches first."""
+        return super(ListModel, self).index(item)
+    
+    def clear(self):
+        """Clears entire model and emits layout changed"""
+        self.layoutAboutToBeChanged.emit()
+        self._container = self._container[:]
+        self.layoutChanged.emit()
+        self.ListCleared.emit()
+
+    # Other Python Special Methods
     def __str__(self):
         """Returns the __str__ of the underlying list."""
         return self._container.__str__()
@@ -171,50 +278,7 @@ class ListModel(QtCore.QAbstractListModel):
     def __ne__(self, *args):
         """Returns whether the underlying list is not equal to another list."""
         return self._container.__ne__(*args)
-
-    # Python Sequence Methods
-    def __len__(self, *args):
-        """Returns the length of the underlying list."""
-        return self._container.__len__(*args)
     
-    def __contains__(self, *args):
-        """Returns whether or not an item is in the underlying list."""
-        return self._container.__contains__(*args)
-    
-    def _convert_idx(self, idx):
-        """To be compatable with Qt objects, ensure the index is a positive number.
-        Used for python index operations."""
-        if idx < 0:
-            #idx -1 on a length 10 list is the same as idx 9 (10 + -1)
-            idx = len(self) + idx
-        return idx
-
-    def __getitem__(self, idx):
-        idx = self._convert_idx(idx)
-        return self._container.__getitem__(idx)
-
-    def __setitem__(self, idx, value):
-        idx = self._convert_idx(idx)
-        previous = self._container[idx]
-        ret_value = self._container.__setitem__(idx, value)
-        index = self.index(idx, 0)
-        self.dataChanged.emit(index, index)
-        self.ListChanged.emit(idx, previous)
-        return ret_value
-
-    def __delitem__(self, idx):
-        """Deletes an item from the underlying list and any associated metadata,
-        then updates the model."""
-        idx = self._convert_idx(idx)
-        previous = self._container[idx]
-        parent = QtCore.QModelIndex()
-        self.beginRemoveRows(parent, idx, idx) 
-        ret_value = self._container.__delitem__(idx)
-        self.container_meta.__delitem__(idx)
-        self.endRemoveRows()
-        self.ListChanged.emit(idx, previous)
-        return ret_value
-
     #TODO methods
     def __setslice__ (self, *args):
         """We disable slice setting until we can handle
@@ -224,45 +288,4 @@ class ListModel(QtCore.QAbstractListModel):
     def __delslice__ (self, *args):
         """We disable slice deleting until we can handle
         alerting the datamodel of the changes"""
-        raise NotImplementedError("No slice deleting functionality.")
-
-
-    #Reimplimenting popular python list methods.
-    def remove(self, item):
-        """Removes the first occurence of an item."""
-        idx = self._container.index(item)
-        del self[idx]
-
-    def pop(self, idx=-1):
-        """Takes an item at the 'idx' index from the list and returns it."""
-        item_to_remove = self._container[idx]
-        del self[idx]
-        return item_to_remove
-
-    def clear(self):
-        """Clears entire model and emits layout changed"""
-        self.layoutAboutToBeChanged.emit()
-        self._container = self._container[:]
-        self.layoutChanged.emit()
-        self.ListCleared.emit()
-
-    def insert(self, idx, value):
-        previous = None
-        try:
-            previous = self._container[idx]
-        except IndexError:
-            pass
-        parent = QtCore.QModelIndex()
-        self.beginInsertRows(parent, idx, idx)
-        ret_value = self._container.insert(idx, value)
-        self.container_meta.insert(idx, ItemMetaData())
-        self.endInsertRows()
-        self.ListChanged.emit(idx, previous)
-        return ret_value
-
-    def append(self, value):
-        return self.insert(len(self._container), value)
-
-    def get_list(self):
-        """Returns a new python list version of the container."""
-        return list(self._container)
+        raise NotImplementedError("No slice deleting functionality.") 

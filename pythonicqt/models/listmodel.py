1842f1ccd7b72894d8df9b6d80b776ada11cd6a1
Digirolamo <CDigirolamo@DigiSpade.com>
2015-08-31 12:47:01 -0400

    Refactored listmodel to include more qabstractlistmodel features.

--- pythonicqt/models/listmodel.py
+++ pythonicqt/models/listmodel.py
@@ -1,146 +1,139 @@
-"""This module contains the Data structures and classes
-that are used to impliment a python list like QAbstractListmodel.
+"""This module contains the class and data structures that make
+the ListModel class.
+
 """
-import copy
-from collections import MutableSequence
+
 import six
-from six.moves import xrange
+from collections import MutableSequence
 from PySide import QtCore
 from pythonicqt.baseclasses import QtMetaStitch
-
-class ItemRoleData(dict):
-    """Contains flags and roles"""
+from pythonicqt.models.modelitems import ItemContainer
 
 @six.add_metaclass(QtMetaStitch)
-class BasicListModel(QtCore.QAbstractListModel):
-    """An implimentation of a QAbstractListModel using a python list.
+class BaseListModel(QtCore.QAbstractListModel):
+    """"A working implimentation of QAbstractListModel.
 
-        self._container is a python list that holds the data of this
-        QAbstractListModel. You can use this model as-is, but its
-        primary purpose is the ListModel subclass of it.
+    This class is a implimentation of the abstract interface of QAbstractListModel.
+    self._container is an internal python list that holds the data of this
+    QAbstractListModel. Internally, each value of the list is actually contained in
+    instance of item_factory. The intention of thie class is to impliment the Qt
+    QAbstractList model methods fully. 
 
-    Note:
-        This object emits two signals in addition to the signals emited by
-        QAbstractListModel.
-        ListChanged (Signal(int, object)): A Qt Signal that emits when a value at
-            an index in the list changes. Emits the index and previous value changed.
-        ListCleared (Signal()): Emited when the list is cleared.
-
-    Attributes:
-        default_flags (ItemFlag): The default flags that all of the items return.
-            Change an instance variable of the same name, or subclass this class
-            and redefine this attribute if you want to use different flags.
-        default_roles (dict[ItemRole]): The default roles that all of the items return.
+    You can use this model as-is, but in pythonicqt the primary purpose is 
+    the ListModel subclass of it.
 
     Keyword Args:
         container (Optional[list]): The first parameter is a list if you want to instantiate
             an instance with starting values. If provided, a copy of the list is created.
+        item_factory(object): The item container that handles item roles and flags along
+            with the data.
+    
     """
-    #So future objects can inherit
-    default_flags = (QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable | 
-                     QtCore.Qt.ItemIsDragEnabled |  QtCore.Qt.ItemIsSelectable)
-    default_roles = {}
 
-    def __init__(self, container=None):
-        QtCore.QAbstractListModel.__init__(self)
+    def __init__(self, container=None, item_factory=ItemContainer):
+        super(BaseListModel, self).__init__()
         if container is None:
             container = []
-        self._container = container
-        self.container_meta = [ItemRoleData() for e in range(len(container))] #index to role/flag index
+        self._item_factory = item_factory
+        self._container = [item_factory(e) for e in container] 
 
-    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-        """Class methods are similar to regular functions.
+
+    def columnCount(self, parent=None):
+        """Returns 1 because lists do not have multiple columns.
+
         Note:
             This method overrides the virtual function of it's parent.
+
         """
-        if orientation == QtCore.Qt.Vertical:
-            return unicode(section)
-        else:
-            return unicode(section)
+        return 1
 
     def rowCount(self, parent=None):
         """Returns the length of the underlying list.
+
         Note:
             This method overrides the virtual function of it's parent.
+
         """
         if parent is None:
             return 0
         return len(self._container)
-    
-    def columnCount(self, parent=None):
-        """Returns 1 because lists do not have multiple columns.
+
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        """Just retuerns the section of the header, lists do not usually have headers.
+
         Note:
             This method overrides the virtual function of it's parent.
+
         """
-        return 1
-    
-    def data(self, index, role=QtCore.Qt.DisplayRole):
-        """Returns the data of a role at a specific index of the list.
-        If the role is DisplayRole or EditRole, returns the value located
-        in the underlying list at the index. Else returns the role of the
-        individual item if the user has set it. Else returns the default
-        role data located in the class attribute if it exists.
-        Args:
-            index (QModelIndex):
-            role (Optional[ItemRole]):
-        Note:
-            This method overrides the virtual function of it's parent.
-        """
-        if not index.isValid():
-            return None
-        row, column = index.row(), index.column()
-        if role == QtCore.Qt.DisplayRole:
-            return unicode(self._container[row])
-        elif role == QtCore.Qt.EditRole: 
-            return self._container[row]
-        item_metadata = self.container_meta[row]
-        try:
-            return item_metadata[role]
-        except KeyError as e:
-            return None
+        return unicode(section)
 
     def setData(self, index, value, role=QtCore.Qt.EditRole):
         """Sets the data of a role at a specific index of the list.
         If the role is DisplayRole or EditRole, sets the value located
         in the underlying list at the index. Else sets the role of the
         individual item specifically.
+
+        This method is expanded to also accept a new role, you can set item
+        flags by passing in the role QtCore.Qt.ItemFlags.
+
         Args:
             index (QModelIndex):
             role (Optional[ItemRole]):
+
         Note:
             This method overrides the virtual function of it's parent.
+
+        Raises:
+            TypeError: if role is not a instance of ItemDataRole or specifically QtCore.Qt.ItemFlags
+
+        """
+        if not index.isValid():
+            return None
+        row = index.row()
+        self._container[row][role] = value
+        self.dataChanged.emit(index, index)
+        return True
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        """Returns the data of a role at a specific index of the list.
+        If the role is DisplayRole or EditRole, returns the value located
+        in the underlying list at the index. Else returns the role of the
+        individual item if the user has set it. Else returns the default
+        role data located in the class attribute if it exists.
+
+        Args:
+            index (QModelIndex):
+            role (Optional[ItemRole]):
+
+        Note:
+            This method overrides the virtual function of it's parent.
+
         """
         if not index.isValid():
             return None
         row, column = index.row(), index.column()
-        if role == QtCore.Qt.EditRole:
-            self[row] = value
-            return True
+        try:
+            return self._container[row][role]
+        except KeyError as e:
+            return None
     
+    #todo docs
     def flags(self, index):
-        """If no specific flags exist for an item, returns the default
-        flags of attribute self.default_flags.
+        """Returns the QtCore.Qt.ItemFlags of the item at the index.
+
+        You can set item flags using the setData method.
         
         Note:
             This method overrides the virtual function of it's parent.
-        """
-        row = index.row()
-        item_metadata = self.container_meta[row]
-        try:
-            return item_metadata.flags
-        except (KeyError, AttributeError):
-            pass
-        return self.default_flags
 
-    #TODO
-    def set_default_meta(self, role, value):
-        """"""
-        
-    def set_meta(self, idx, role, value):
-        """Returns a list that"""
-        self.container_meta[idx][role] = value
-        index = self.index(idx, 0)
-        self.dataChanged.emit(index, index)
+        """
+        if not index.isValid():
+            return None
+        row, column = index.row(), index.column()
+        try:
+            return self._container[row][QtCore.Qt.ItemFlags]
+        except KeyError as e:
+            return None
 
     def __copy__(self, deep_copy_memo=None):
         """Makes a shallow copy of the list and returns a new model of it.
@@ -149,6 +142,7 @@ class BasicListModel(QtCore.QAbstractListModel):
             deep_copy_memo (Optional[dict]): Only to be used by a __deepcopy__
                 implimentation. If deep_copy_memo is not None, it should be the 
                 memo argument of __deep__ copy. 
+
         """
         cls = self.__class__
         if deep_copy_memo is None:
@@ -157,11 +151,6 @@ class BasicListModel(QtCore.QAbstractListModel):
         else:
             container = copy.deepcopy(self._container, deep_copy_memo)
         new_instance = cls(self._container)
-        #only need to copy if the instance set the flags/roles
-        if hasattr(self.__dict__, 'default_flags'):
-            new_instance.default_flags = self.default_flags
-        if hasattr(self.__dict__, 'default_roles'):
-            new_instance.default_roles = self.default_roles
         return new_instance
 
     def __deepcopy__(self, memo):
@@ -169,7 +158,7 @@ class BasicListModel(QtCore.QAbstractListModel):
         return self.__copy__(deep_copy_memo=memo)
 
 
-class ListModel(MutableSequence, BasicListModel):
+class ListModel(MutableSequence, BaseListModel):
     """A python list like implimentation of a QAbstractListModel.
     
     Instances of this class function as both a QAbstractListModel and
@@ -183,6 +172,12 @@ class ListModel(MutableSequence, BasicListModel):
         ListModel.index_of is the method for MutableSequence.index. 
         ListModel.index is the QAbstractListModel.index method.
         
+        This object emits two signals in addition to the signals emited by
+            QAbstractListModel.
+        ListChanged (Signal(int, object)): A Qt Signal that emits when a value at
+            an index in the list changes. Emits the index and previous value changed.
+        ListCleared (Signal()): Emited when the list is cleared.
+
     """
     ListChanged = QtCore.Signal(int, object) 
     ListCleared = QtCore.Signal() 
@@ -191,6 +186,9 @@ class ListModel(MutableSequence, BasicListModel):
     def _convert_idx(self, idx):
         """To be compatable with Qt objects, ensure the index is a positive number.
         Used for python index operations."""
+        #TODO slicing
+        if isinstance(idx, slice):
+            raise NotImplementedError("No slice setting functionality yet.")
         if idx < 0:
             #idx -1 on a length 10 list is the same as idx 9 (10 + -1)
             idx = len(self) + idx
@@ -203,8 +201,7 @@ class ListModel(MutableSequence, BasicListModel):
         Note:
             This method is an abstract method required for MutableSequence.
         """
-        idx = self._convert_idx(idx)
-        return self._container.__getitem__(idx)
+        return self._container.__getitem__(idx).data
 
     def __setitem__(self, idx, value):
         """Sets the data located the the index or slice in the underlying list.
@@ -213,13 +210,11 @@ class ListModel(MutableSequence, BasicListModel):
             This method is an abstract method required for MutableSequence.
         """
         idx = self._convert_idx(idx)
-        idx = self._convert_idx(idx)
-        previous = self._container[idx]
-        ret_value = self._container.__setitem__(idx, value)
         index = self.index(idx, 0)
-        self.dataChanged.emit(index, index)
+        previous = self._container[idx].data
+        self.setData(index, value)
         self.ListChanged.emit(idx, previous)
-        return ret_value
+        return 
 
     def __delitem__(self, idx):
         """Deletes an item from the underlying list and any associated metadata,
@@ -229,11 +224,10 @@ class ListModel(MutableSequence, BasicListModel):
             This method is an abstract method required for MutableSequence.
         """
         idx = self._convert_idx(idx)
-        previous = self._container[idx]
+        previous = self._container[idx].data
         parent = QtCore.QModelIndex()
         self.beginRemoveRows(parent, idx, idx) 
         ret_value = self._container.__delitem__(idx)
-        self.container_meta.__delitem__(idx)
         self.endRemoveRows()
         self.ListChanged.emit(idx, previous)
         return ret_value
@@ -253,13 +247,12 @@ class ListModel(MutableSequence, BasicListModel):
             This method is an abstract method required for MutableSequence."""
         previous = None
         try:
-            previous = self._container[idx]
+            previous = self._container[idx].data
         except IndexError:
             pass
         parent = QtCore.QModelIndex()
         self.beginInsertRows(parent, idx, idx)
-        ret_value = self._container.insert(idx, value)
-        self.container_meta.insert(idx, ItemRoleData())
+        ret_value = self._container.insert(idx, self._item_factory(value))
         self.endInsertRows()
         self.ListChanged.emit(idx, previous)
         return ret_value
@@ -290,30 +283,20 @@ class ListModel(MutableSequence, BasicListModel):
     # Other Python Special Methods
     def __str__(self):
         """Returns the __str__ of the underlying list."""
-        return self._container.__str__()
+        return list(self).__str__()
     
     def __repr__(self):
         """The representation of the object, does not include any of the metadata
         such as flags and roles."""
         class_name = self.__class__.__name__
-        list_repr = self._container.__repr__()
-        return "{}({})".format(class_name, list_repr)
+        list_repr = list(self).__repr__()
+        factory_name = self._item_factory.__name__
+        return "{}({}, item_factory={})".format(class_name, list_repr, factory_name)
 
     def __eq__(self, *args):
         """Returns whether the underlying list equals another list."""
-        return self._container.__eq__(*args)
+        return list(self).__eq__(*args)
 
     def __ne__(self, *args):
         """Returns whether the underlying list is not equal to another list."""
-        return self._container.__ne__(*args)
-    
-    #TODO methods
-    def __setslice__ (self, *args):
-        """We disable slice setting until we can handle
-        alerting the datamodel of the changes"""
-        raise NotImplementedError("No slice setting functionality.")
-        
-    def __delslice__ (self, *args):
-        """We disable slice deleting until we can handle
-        alerting the datamodel of the changes"""
-        raise NotImplementedError("No slice deleting functionality.") 
+        return list(self).__ne__(*args)
---
+++ pythonicqt/models/modelitems.py
@@ -0,0 +1,64 @@
+"""This module should contain all of the types of containers that
+hold the flags and data roles of items in models.
+
+"""
+
+from PySide import QtCore
+
+
+class ItemContainer(dict):
+    """A dictionary used to hold a QAbstractModel item's data and flags.
+
+    In Qt models, every item in a model can have specific data and flags.
+    Instances of this class contains flags and roles of a specific item.
+    If a specific item does not have a set role, tries to return a default role
+    if the class variable default_data has any. 
+    
+    EditRole usualy returns the actual data, DisplayRole returns a unicode version
+    of the data.
+    
+    Attributes:
+        default_data (ItemFlag): The default flags and  roles that all of the items return.
+            Change an instance variable of the same name, or subclass this class
+            and redefine this attribute if you want to use different flags.
+    """
+    default_data = {
+        QtCore.Qt.ItemFlags: 
+            (QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | 
+             QtCore.Qt.ItemIsEditable)
+        }
+
+    def __init__(self, item_data, role=QtCore.Qt.EditRole):
+        self[role] = item_data
+
+    @property
+    def data(self):
+        """Convenient property that returns the default data.
+
+            Rather 
+        """
+        return self[QtCore.Qt.EditRole]
+
+    def __getitem__(self, key):
+        """Returns the data or flags of this dictionary item.
+       
+        Returns:
+            ItemData or ItemRole: If the key is in this instances dictionary, returns
+                that value. Otherwise, looks if the key is in the default_data dictionary
+                and returns that value. Else raises KeyError.
+        """
+        if key in self:
+            return super(ItemContainer, self).__getitem__(key)
+        elif key == QtCore.Qt.DisplayRole:
+            return unicode(self.data)
+        else:
+            #This raises a KeyError if no default exists.
+            return self.default_data[key]
+
+    def __setitem__(self, key, value):
+        """Sets the data or flags of the item.
+        
+        Raises:
+            TypeError: if role is not a instance of ItemDataRole or specifically QtCore.Qt.ItemFlags
+        """
+        super(ItemContainer, self).__setitem__(key, value)
